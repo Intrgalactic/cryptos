6a2b03e5efb6d74e2397ffc182e47eabea920524
Mateusz <mateuszrustowiczyt@gmail.com>
2023-07-06 15:05:41 +0200

    added appearance for pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -6,19 +6,14 @@ import Loader from "layouts/loader";
 import PricingSection from "layouts/pricing-section";
 import { FaqRecord } from "components/faq-record";
 import { motion } from "framer-motion";
-export default function Pricing({isLoading,setIsLoading}) {
+export default function Pricing({ isLoading, setIsLoading }) {
     useLoader(setIsLoading);
     return (
         <div className="pricing-app">
-            <Header/>
-            <PricingSection/>
+            <Header />
+            <PricingSection />
             <Faq heading={["frequently asked questions", <br />, "about subscription"]} class="pricing-faq">
-            <FaqRecord heading="Lorem ipsum dolor sit amet, consectetur adipiscing elit?">
-                    <>
-                        <hr />
-                        <motion.p animate={{ y: 0 }} initial={{ y: -200 }} transition={{ type: "linear" }}>It is a long established fact that a reader will be distracted by the readable content</motion.p>
-                    </>
-                </FaqRecord>
+
                 <FaqRecord heading="Lorem ipsum dolor sit amet, consectetur adipiscing elit?">
                     <>
                         <hr />
@@ -37,8 +32,15 @@ export default function Pricing({isLoading,setIsLoading}) {
                         <motion.p animate={{ y: 0 }} initial={{ y: -200 }} transition={{ type: "linear" }}>It is a long established fact that a reader will be distracted by the readable content</motion.p>
                     </>
                 </FaqRecord>
+                <FaqRecord heading="Lorem ipsum dolor sit amet, consectetur adipiscing elit?">
+                    <>
+                        <hr />
+                        <motion.p animate={{ y: 0 }} initial={{ y: -200 }} transition={{ type: "linear" }}>It is a long established fact that a reader will be distracted by the readable content</motion.p>
+                    </>
+                </FaqRecord>
+
             </Faq>
-            <Footer/>
+            <Footer />
             {isLoading &&
                 <Loader />}
         </div>
